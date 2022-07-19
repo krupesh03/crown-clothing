f7a2c0f816316627aa21b1849c6cfcc4f3875b5d
Krupesh Bhosle <bhoslekrupesh03@gmail.com>
2022-07-19 20:45:45 +0530

    collection item added

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
         <h1 className="title">{ title }</h1>
         <div className="preview">
             {
-                items.filter((item, index) => index < 4).map( (item) => (
-                    <div key={item.id}> { item.name } </div>
+                items.filter((item, index) => index < 4).map( ({id, ...otherItemProps}) => (
+                    <CollectionItem key={id} { ...otherItemProps } />
                 ))
             }
         </div>
