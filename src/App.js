5e6c6852a8691e18160d830ef781d94cba82d1f1
Krupesh Bhosle <bhoslekrupesh03@gmail.com>
2022-07-20 20:51:03 +0530

    sign in and sign up in progress

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<HomePage />} />
           <Route path='/shop' element={ <ShopPage /> } />
+          <Route path='/signin' element={ <SignInAndSignUp /> } />
         </Routes>
       </Router>
     </div>
