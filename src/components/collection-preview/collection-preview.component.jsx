cdd65dc86779bb86fb9b3d5f8a70d10175eb266e
Krupesh Bhosle <bhoslekrupesh03@gmail.com>
2022-10-17 20:59:48 +0530

    added Add to cart button and implements cart items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,11 +4,11 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
-        <h1 className="title">{ title }</h1>
+        <h1 className="title">{ title.toUpperCase() }</h1>
         <div className="preview">
             {
-                items.filter((item, index) => index < 4).map( ({id, ...otherItemProps}) => (
-                    <CollectionItem key={id} { ...otherItemProps } />
+                items.filter((item, index) => index < 4).map( (item) => (
+                    <CollectionItem key={item.id} item={item} />
                 ))
             }
         </div>
